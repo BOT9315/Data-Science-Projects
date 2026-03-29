cd48946cc02fc2cbc0899e845b5b69333212b828
Ankush <akrk053@gmail.com>
2026-03-29 13:11:20 +0530

    Refactor input handling and intersection logic

--- HackerRank_TwoStrings.py
+++ HackerRank_TwoStrings.py
@@ -10,19 +10,16 @@
 # Output:
 # For each test case, display YES (in a newline), if there is a common substring. 
 # Otherwise, display NO
-
 n = int(input().strip())
 a = []
-for a_i in range(2*n):
-   a_t = [a_temp for a_temp in input().strip().split(' ')][0]
-   a.append(a_t)
 
-for i in range(2*n):
-    if (i%2==0):
-        s1=set(a[i])
-        s2=set(a[i+1])
-        S=s1.intersection(s2) #intersection of subset -> return the same characters !!
-        if (len(S)==0):
-            print("NO")
-        else:
-            print("YES")
+for _ in range(2 * n):
+    a.append(input().strip())  # read the full line as one string
+
+for i in range(0, 2 * n, 2):  # step by 2 to get pairs
+    s1 = set(a[i])
+    s2 = set(a[i + 1])
+    if s1.intersection(s2):
+        print("YES")
+    else:
+        print("NO")
